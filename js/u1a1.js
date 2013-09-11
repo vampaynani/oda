fa7429fbe73bebbe490da0d81961746574d8a0b0
Luisana Martín <luisana.martin@gmail.com>
2013-09-11 17:26:40 -0500

    imagenes en codigo

--- js/u1a1.js
+++ js/u1a1.js
@@ -1,30 +1,15 @@
 var stage, imgurl="imgs/";
 
-var imgBrat1 = new Image(), 
-    imgBrat2 = new Image(), 
-    imgBrat3 = new Image(),
-    imgSideBack = new Image(),
-    imgSideBratz1 = new Image(), imgSideBratz2 = new Image(), imgSideBratz3 = new Image(),
-    imgSideWorld1 = new Image(), imgSideWorld2 = new Image(), imgSideWorld3 = new Image(),
-    imgSideAtt1 = new Image(), imgSideAtt2 = new Image(), imgSideAtt3 = new Image(),
-    imgSideLook1 = new Image(), imgSideLook2 = new Image(), imgSideLook3 = new Image(),
-    imgFlechita = new Image(), imgFlechitaArriba = new Image(), imgFlechitaAbajo = new Image();
+var imgHeader = new Image(),
+    imgNube1 = new Image(),
+    imgNube2 = new Image(),
+    imgNube3 = new Image(),
+    imgInstrucciones = new Image(),
+    imgGlobito = new Image();
 
-var bratzSps, bratzAnim;
-var worldSps, worldAnim;
-var attitudeSps, attitudeAnim;
-var labioJadeSps, labioJadeAnim;
-var labioYasminSps, labioYasminAnim;
-var labioCloeSps, labioCloeAnim;
-var ropaJadeSps, ropaJadeAnim;
-var ropaYasminSps, ropaYasminAnim;
-var ropaCloeSps, ropaCloeAnim;
-var zapatosJadeSps, zapatosJadeAnim;
-var zapatosYasminSps, zapatosYasminAnim;
-var zapatosCloeSps, zapatosCloeAnim;
-var plusSps;
-var sideBratz, sideWorlds, sideAttitude, sideLook;
-var sideOut, sideIn, arrowIn;
+var personajeSps, personajeAnim;
+
+var nubeUnoContenedor, nubeDosContenedor, nubeTresContenedor;
 var progressLabel, preload;
 
 function oda() {
@@ -32,60 +17,22 @@ function oda() {
   preload.addEventListener('complete', handleComplete);
   preload.addEventListener('progress', handleProgress);
     preload.loadManifest([
-      {id: 'isb', src: imgurl + 'side_back.png'},
-      {id: 'isbr1', src: imgurl + 'side_bratz_jade.png'},
-      {id: 'isbr2', src: imgurl + 'side_bratz_yasmin.png'},
-      {id: 'isbr3', src: imgurl + 'side_bratz_cloe.png'},
-      {id: 'isw1', src: imgurl + 'side_backs_1.png'},
-      {id: 'isw2', src: imgurl + 'side_backs_2.png'},
-      {id: 'isw3', src: imgurl + 'side_backs_3.png'},
-      {id: 'isa3', src: imgurl + 'side_attitude_compartida.png'},
-      {id: 'isa1', src: imgurl + 'side_attitude_suenos.png'},
-      {id: 'isa2', src: imgurl + 'side_attitude_unica.png'},
-      {id: 'isl1', src: imgurl + 'side_chars_1.png'},
-      {id: 'isl2', src: imgurl + 'side_chars_2.png'},
-      {id: 'isl3', src: imgurl + 'side_chars_3.png'},
-        {id: 'fl', src: imgurl + 'flecharosa.png'},
-        {id: 'fl2', src: imgurl + 'arrow_up.png'},
-        {id: 'fl3', src: imgurl + 'arrow_down.png'},
-      {id: 'br1', src: imgurl + 'bratz_jade.png'},
-      {id: 'br2', src: imgurl + 'bratz_yasmin.png'},
-      {id: 'br3', src: imgurl + 'bratz_cloe.png'},
-      {id: 'fo1', src: imgurl + 'fondo_1.png'},
-      {id: 'fo2', src: imgurl + 'fondo_2.png'},
-      {id: 'fo3', src: imgurl + 'fondo_3.png'},
-      {id: 'at3', src: imgurl + 'attitude_compartida.png'},
-      {id: 'at1', src: imgurl + 'attitude_suenos.png'},
-      {id: 'at2', src: imgurl + 'attitude_unica.png'},
+      {id: 'n1', src: imgurl + 'nube1.png'},
+      {id: 'n2', src: imgurl + 'nube2.png'},
+      {id: 'n3', src: imgurl + 'nube3.png'},
 
-        {id: 'lb0', src: imgurl + 'vacio.png'},
-        
-        {id: 'lb1', src: imgurl + 'labio_2.png'},
-        {id: 'lb2', src: imgurl + 'labio_3.png'},
-        {id: 'lby1', src: imgurl + 'labio_yasmin_1.png'},
-        {id: 'lby2', src: imgurl + 'labio_yasmin_2.png'},
-        {id: 'lbc1', src: imgurl + 'labio_cloe_1.png'},
-        {id: 'lbc2', src: imgurl + 'labio_cloe_2.png'},
+      {id: 'inst', src: imgurl + 'texto_look.png'},
+      {id: 'glob', src: imgurl + 'cuadro_naranja.png'},
+      {id: 'head', src: imgurl + 'head.png'},
 
-        {id: 'rj1', src: imgurl + 'ropa_jade_1.png'},
-        {id: 'rj2', src: imgurl + 'ropa_jade_2.png'},
-        {id: 'ry1', src: imgurl + 'ropa_yasmin_1.png'},
-        {id: 'ry2', src: imgurl + 'ropa_yasmin_2.png'},
-        {id: 'rc1', src: imgurl + 'ropa_cloe_1.png'},
-        {id: 'rc2', src: imgurl + 'ropa_cloe_2.png'},
+      {id: 'per1', src: imgurl + 'niños_cantando.png'},
+      {id: 'per2', src: imgurl + 'niños_jugando.png'},
+      {id: 'per3', src: imgurl + 'niña_jugando.png'},
+      {id: 'per4', src: imgurl + 'niños_leyendo.png'},
+      {id: 'per5', src: imgurl + 'niño_flauta.png'},
 
-        {id: 'zj1', src: imgurl + 'zapatos_jade_1.png'},
-        {id: 'zj2', src: imgurl + 'zapatos_jade_2.png'},
-        {id: 'zy1', src: imgurl + 'zapatos_yasmin_1.png'},
-        {id: 'zy2', src: imgurl + 'zapatos_yasmin_2.png'},
-        {id: 'zc1', src: imgurl + 'zapatos_cloe_1.png'},
-        {id: 'zc2', src: imgurl + 'zapatos_cloe_2.png'},
 
-        {id: 'pl1', src: imgurl + 'plus_0.png'},
-        {id: 'pl2', src: imgurl + 'plus_1.png'},
-        {id: 'pl3', src: imgurl + 'plus_2.png'},
-        {id: 'pl4', src: imgurl + 'plus_3.png'},
-
+     
     ]);
 }
 function handleProgress(){
@@ -100,295 +47,106 @@ function handleComplete(){
     createjs.Ticker.addListener(window);
   createjs.Ticker.setFPS(60);
 }
+ 
 function initAssets(){
-    imgSideBack = preload.getResult('isb');
-    imgSideBratz1 = preload.getResult('isbr1');
-    imgSideBratz2 = preload.getResult('isbr2');
-    imgSideBratz3 = preload.getResult('isbr3');
-    imgSideWorld1 = preload.getResult('isw1');
-    imgSideWorld2 = preload.getResult('isw2');
-    imgSideWorld3 = preload.getResult('isw3');
-    imgSideWorld4 = preload.getResult('isw3');
-    imgSideAtt1 = preload.getResult('isa1');
-    imgSideAtt2 = preload.getResult('isa2');
-    imgSideAtt3 = preload.getResult('isa3');
-    imgSideAtt4 = preload.getResult('isa3');
-    imgSideLook1 = preload.getResult('isl1');
-    imgSideLook2 = preload.getResult('isl2');
-    imgSideLook3 = preload.getResult('isl3');
-    imgSideLook4 = preload.getResult('isl3');
-    imgFlechita = preload.getResult('fl');
-    imgFlechitaArriba = preload.getResult('fl2');
-    imgFlechitaAbajo = preload.getResult('fl3');
 
-    labioJadeSps = new createjs.SpriteSheet({
-        images: [preload.getResult('lb0'), preload.getResult('lb1'), preload.getResult('lb2')],
+    imgHeader = preload.getResult('head');
+    imgNube1 = preload.getResult('n1');
+    imgNube2 = preload.getResult('n2');
+    imgNube3 = preload.getResult('n3');
+    imgInstrucciones = preload.getResult('inst');
+    imgGlobito = preload.getResult('glob');
+   
+    personajeSps = new createjs.SpriteSheet({
+        images: [preload.getResult('per1'), preload.getResult('per2'), preload.getResult('per3'), preload.getResult('per4'), preload.getResult('per5')],
         frames: {width: 360, height: 465, regX: 180, regY: 233},
       
     });  
-    labioYasminSps = new createjs.SpriteSheet({
-        images: [preload.getResult('lb0'), preload.getResult('lby1'), preload.getResult('lby2')],
-        frames: {width: 360, height: 465, regX: 180, regY: 233},
-      
-    });      
-    labioCloeSps = new createjs.SpriteSheet({
-        images: [preload.getResult('lb0'), preload.getResult('lbc1'), preload.getResult('lbc2')],
-        frames: {width: 360, height: 465, regX: 180, regY: 233},
     
-    });     
+    
 
-    ropaJadeSps = new createjs.SpriteSheet({
-        images: [preload.getResult('lb0'), preload.getResult('rj1'), preload.getResult('rj2')],
-        frames: {width: 360, height: 465, regX: 180, regY: 233},
-      
-    });     
-    ropaYasminSps = new createjs.SpriteSheet({
-        images: [preload.getResult('lb0'), preload.getResult('ry1'), preload.getResult('ry2')],
-        frames: {width: 360, height: 465, regX: 180, regY: 233},
-      
-    });     
-    ropaCloeSps = new createjs.SpriteSheet({
-        images: [preload.getResult('lb0'), preload.getResult('rc1'), preload.getResult('rc2')],
-        frames: {width: 360, height: 465, regX: 180, regY: 233},
-      
-    });     
-    zapatosJadeSps = new createjs.SpriteSheet({
-        images: [preload.getResult('lb0'), preload.getResult('zj1'), preload.getResult('zj2')],
-        frames: {width: 360, height: 465, regX: 180, regY: 233},
-      
-    });     
-    zapatosYasminSps = new createjs.SpriteSheet({
-        images: [preload.getResult('lb0'), preload.getResult('zy1'), preload.getResult('zy2')],
-        frames: {width: 360, height: 465, regX: 180, regY: 233},
-      
-    });     
-    zapatosCloeSps = new createjs.SpriteSheet({
-        images: [preload.getResult('lb0'), preload.getResult('zc1'), preload.getResult('zc2')],
-        frames: {width: 360, height: 465, regX: 180, regY: 233},
-      
-    });     
-
-    bratzSps = new createjs.SpriteSheet({
-        images: [preload.getResult('br1'), preload.getResult('br2'), preload.getResult('br3')],
-        frames: {width: 360, height: 465, regX: 180, regY: 233},
-        animations: {
-            jade: 0,
-            yasmin: 1,
-            cloe: 2
-        }
-    });
+    personajeAnim = new createjs.BitmapAnimation(personajeSps);
+   
   
-    worldSps = new createjs.SpriteSheet({
-        images: [preload.getResult('fo1'), preload.getResult('fo2'), preload.getResult('fo3')],
-        frames: {width: 547, height: 670, regX: 274, regY: 335}
-    });
-    plusSps = new createjs.SpriteSheet({
-        images: [ preload.getResult('pl1'), preload.getResult('pl2'), preload.getResult('pl3'), preload.getResult('pl4')],
-        frames: {width: 547, height: 670, regX: 0, regY: 0}
-    });
-    attitudeSps = new createjs.SpriteSheet({
-     images: [preload.getResult('at1'), preload.getResult('at2'), preload.getResult('at3')],
-     frames: {width: 173, height: 87, regX: 86, regY: 43} 
-    });
-    bratzAnim = new createjs.BitmapAnimation(bratzSps);
-    worldAnim = new createjs.BitmapAnimation(worldSps);
-    plusAnim = new createjs.BitmapAnimation(plusSps);
-    attitudeAnim = new createjs.BitmapAnimation(attitudeSps);
-  
-    labioJadeAnim = new createjs.BitmapAnimation(labioJadeSps);
-    labioYasminAnim = new createjs.BitmapAnimation(labioYasminSps);
-    labioCloeAnim = new createjs.BitmapAnimation(labioCloeSps);
-
-    ropaJadeAnim = new createjs.BitmapAnimation(ropaJadeSps);
-    ropaYasminAnim = new createjs.BitmapAnimation(ropaYasminSps);
-    ropaCloeAnim = new createjs.BitmapAnimation(ropaCloeSps);
-
-    zapatosJadeAnim = new createjs.BitmapAnimation(zapatosJadeSps);
-    zapatosYasminAnim = new createjs.BitmapAnimation(zapatosYasminSps);
-    zapatosCloeAnim = new createjs.BitmapAnimation(zapatosCloeSps);
 }
 
 function setStage(){
     stage = new createjs.Stage("stage");
-    sideOut = stage.canvas.width;
-    sideIn = stage.canvas.width - imgSideBack.width;
-    arrowIn = stage.canvas.width - imgFlechita.width;
-        
+  
+
     createjs.Touch.enable(stage);
     stage.enableMouseOver();
     
-    setWorld();
-}
-function setWorld(){
+    personajeAnim.currentFrame = 0;
+    stage.addChild(personajeAnim);
 
+    setNube1();
+    setNube2();
+    setNube3();
 }
-function setBratzSide(){
-    var sideBack = new createjs.Bitmap(imgSideBack);
-    var brt1 = new Draggable('brt1', imgSideBratz1, 'bratz', 1, 26, 26);
-    var brt2 = new Draggable('brt2', imgSideBratz2, 'bratz', 2, 26, 190);
-    var brt3 = new Draggable('brt3', imgSideBratz3, 'bratz', 3, 26, 360);
-    sideBratz = new createjs.Container();
+ 
+function setNube1(){
+    var fondo1 = new createjs.Bitmap(imgNube1);
+
+    var palabra1nube1 = new Dragtxt("p1n1", "aqui texto", 20, 20);
+    var palabra2nube1 = new Dragtxt("p2n1", "aqui texto", 20, 20);
+   
+    nubeUnoContenedor = new createjs.Container();
+     
+    nubeUnoContenedor.y = 53;
+    nubeUnoContenedor.x = stage.canvas.width;
     
-    sideBratz.y = 53;
-    sideBratz.x = stage.canvas.width;
-    
-    sideBratz.addChild(sideBack, brt1, brt2, brt3);
-    stage.addChild(sideBratz);
+    nubeUnoContenedor.addChild(fondo1, palabra1nube1, palabra2nube1);
+
+    stage.addChild(nubeUnoContenedor);
 }
 
-function setWorldsSide(){
-  var wld1 = new Draggable('wld1', imgSideWorld1, 'world', 1, 26, 26);
-    var wld2 = new Draggable('wld2', imgSideWorld2, 'world', 2, 26, 190);
-    var wld3 = new Draggable('wld3', imgSideWorld3, 'world', 3, 26, 360);
-    var wld4 = new Draggable('wld4', imgSideWorld4, 'world', 4, 26, 530);
-  sideWorlds = new Scrollable('wld0', stage.canvas.width, 53);
+function setNube2(){
+    var fondo1 = new createjs.Bitmap(imgNube2);
+
+    var palabra1nube2 = new Dragtxt("p1n2", "aqui texto", 20, 20);
+    var palabra2nube2 = new Dragtxt("p2n2", "aqui texto", 20, 20);
+    var palabra3nube2 = new Dragtxt("p3n2", "aqui texto", 20, 20);
+    var palabra4nube2 = new Dragtxt("p4n2", "aqui texto", 20, 20);
   
-  sideWorlds.addCollection(wld1,wld2,wld3,wld4);
-  stage.addChild(sideWorlds);
-}
-function setAttitudeSide(){
-    var sideBack = new createjs.Bitmap(imgSideBack);
-    var att1 = new Draggable('att1', imgSideAtt1, 'attitude', 1, 26, 26);
-    var att2 = new Draggable('att2', imgSideAtt2, 'attitude', 2, 26, 190);
-    var att3 = new Draggable('att3', imgSideAtt3, 'attitude', 3, 26, 360);
-    var att4 = new Draggable('att4', imgSideAtt4, 'attitude', 4, 26, 530);
-    sideAttitude = new Scrollable('att0', stage.canvas.width, 53);
+    nubeDosContenedor = new createjs.Container();
+     
+    nubeDosContenedor.y = 53;
+    nubeDosContenedor.x = stage.canvas.width;
     
-    sideAttitude.addCollection(att1,att2,att3,att4);
-    stage.addChild(sideAttitude);
+    nubeDosContenedor.addChild(fondo1, palabra1nube2, palabra2nube2, palabra3nube2, palabra4nube2);
 
+    stage.addChild(nubeDosContenedor);
 }
-function setLookSide(){
-    var sideBack = new createjs.Bitmap(imgSideBack);
-    var lok1 = new Draggable('lok1', imgSideLook1, 'look', 1, 26, 26);
-    var lok2 = new Draggable('lok2', imgSideLook2, 'look', 2, 26, 190);
-    var lok3 = new Draggable('lok3', imgSideLook3, 'look', 3, 26, 360);
-    var lok4 = new Draggable('lok4', imgSideLook4, 'look', 4, 26, 530);
-    sideLook = new Scrollable('lok0', stage.canvas.width, 53);
+function setNube3(){
+    var fondo1 = new createjs.Bitmap(imgNube2);
+
+    var palabra1nube3 = new Dragtxt("p1n3", "aqui texto", 20, 20);
+    var palabra2nube3 = new Dragtxt("p2n3", "aqui texto", 20, 20);
+    var palabra3nube3 = new Dragtxt("p3n3", "aqui texto", 20, 20);
+    var palabra4nube3 = new Dragtxt("p4n3", "aqui texto", 20, 20);
+    var palabra5nube3 = new Dragtxt("p5n3", "aqui texto", 20, 20);
+  
+    nubeTresContenedor = new createjs.Container();
+
+    nubeTresContenedor.y = 53;
+    nubeTresContenedor.x = stage.canvas.width;
     
-    sideLook.addCollection(lok1,lok2,lok3,lok4);
-    stage.addChild(sideLook);
+    nubeDosContenedor.addChild(fondo1, palabra1nube3, palabra2nube3, palabra3nube3, palabra4nube3, palabra5nube3);
 
+    stage.addChild(nubeTresContenedor);
 }
+
 function setListeners(){
     
 }
 function updateStage(e){
-    switch(e.target.side){
-        case 'bratz':
-            bratzBtn();
-            labioJadeAnim.currentFrame=0;
-            labioYasminAnim.currentFrame=0;
-            labioCloeAnim.currentFrame=0;
-            ropaJadeAnim.currentFrame=0;
-            ropaYasminAnim.currentFrame=0;
-            ropaCloeAnim.currentFrame=0;
-            zapatosJadeAnim.currentFrame=0;
-            zapatosYasminAnim.currentFrame=0;
-            zapatosCloeAnim.currentFrame=0;
-
-            if(e.target.selection == 1) bratzAnim.gotoAndStop('jade');
-            if(e.target.selection == 2) bratzAnim.gotoAndStop('yasmin');
-            if(e.target.selection == 3) bratzAnim.gotoAndStop('cloe');
-            break;
-        case 'look':
-            lookBtn();
-            if(e.target.selection == 1) {
-            if(bratzAnim.currentAnimation == 'jade'){  
-                    labioJadeAnim.currentFrame = labioJadeAnim.currentFrame+1;
-                }
-                if( bratzAnim.currentAnimation == 'yasmin'){  
-                      labioYasminAnim.currentFrame = labioYasminAnim.currentFrame+1;
-                }
-                if( bratzAnim.currentAnimation == 'cloe'){  
-                      labioCloeAnim.currentFrame = labioCloeAnim.currentFrame+1;
-                }
-            }
-            if(e.target.selection == 2) {
-                if(bratzAnim.currentAnimation == 'jade'){  
-                    ropaJadeAnim.currentFrame = ropaJadeAnim.currentFrame+1;
-                }
-                if( bratzAnim.currentAnimation == 'yasmin'){  
-                      ropaYasminAnim.currentFrame = ropaYasminAnim.currentFrame+1;
-                }
-                if( bratzAnim.currentAnimation == 'cloe'){  
-                      ropaCloeAnim.currentFrame = ropaCloeAnim.currentFrame+1;
-                }
-            }
-            if(e.target.selection == 3) {
-                if(bratzAnim.currentAnimation == 'jade'){  
-                    zapatosJadeAnim.currentFrame = zapatosJadeAnim.currentFrame+1;
-                }
-                if( bratzAnim.currentAnimation == 'yasmin'){  
-                      zapatosYasminAnim.currentFrame = zapatosYasminAnim.currentFrame+1;
-                }
-                if( bratzAnim.currentAnimation == 'cloe'){  
-                      zapatosCloeAnim.currentFrame = zapatosCloeAnim.currentFrame+1;
-                }
-            }
-            if(e.target.selection == 4) {
-                      plusAnim.currentFrame = plusAnim.currentFrame+1;
-                console.log("boton cuatro");
-            }
-
-            break;
-        case 'world':
-            worldBtn();
-            worldAnim.currentFrame = e.target.selection - 1;
-            break;
-        case 'attitude':
-            attiBtn();
-          attitudeAnim.currentFrame = e.target.selection - 1;
-          break;
-    }
+    
 }
-function chooseBratz() {
-  flechita.y = 250;
-    if(sideBratz.x == sideIn){
-        TweenLite.fromTo(flechita, 0.5, {x: arrowIn}, {x: sideOut});
-        TweenLite.fromTo(sideBratz, 0.5, {x: sideIn}, {x: sideOut});
-    }else{
-        TweenLite.fromTo(flechita, 0.5, {x: sideOut}, {x: arrowIn});
-        TweenLite.fromTo(sideBratz, 0.5, {x: sideOut}, {x: sideIn});
-    }
-}
-function chooseWorld() {
-  flechita.y = 375;
-    if(sideWorlds.x == sideIn){
-        TweenLite.fromTo(flechita, 0.5, {x: arrowIn}, {x: sideOut});
-        TweenLite.fromTo(sideWorlds, 0.5, {x: sideIn}, {x: sideOut});
-    }else{
-        TweenLite.fromTo(flechita, 0.5, {x: sideOut}, {x: arrowIn});
-        TweenLite.fromTo(sideWorlds, 0.5, {x: sideOut}, {x: sideIn});
-    }
-}
-function chooseAttitude() {
-  flechita.y = 130;
-    if(sideAttitude.x == sideIn){
-        TweenLite.fromTo(flechita, 0.5, {x: arrowIn}, {x: sideOut});
-        TweenLite.fromTo(sideAttitude, 0.5, {x: sideIn}, {x: sideOut});
-    }else{
-        TweenLite.fromTo(flechita, 0.5, {x: sideOut}, {x: arrowIn});
-        TweenLite.fromTo(sideAttitude, 0.5, {x: sideOut}, {x: sideIn});
-    }
-}
-function chooseLook() {
-    flechita.y = 500;
-    if(sideLook.x == sideIn){
-        TweenLite.fromTo(flechita, 0.5, {x: arrowIn}, {x: sideOut});
-        TweenLite.fromTo(sideLook, 0.5, {x: sideIn}, {x: sideOut});
-    }else{
-        TweenLite.fromTo(flechita, 0.5, {x: sideOut}, {x: arrowIn});
-        TweenLite.fromTo(sideLook, 0.5, {x: sideOut}, {x: sideIn});
-    }
-}
-function hideChoosers(){
-    TweenLite.to(flechita, 0.5, {x: sideOut})
-    TweenLite.to(sideBratz, 0.5, {x: sideOut});
-    TweenLite.to(sideWorlds, 0.5, {x: sideOut});
-    TweenLite.to(sideAttitude, 0.5, {x: sideOut});
-    TweenLite.to(sideLook, 0.5, {x: sideOut});
-}
+ 
+ 
 function tick() {
     stage.update();
 }
+
+ 
